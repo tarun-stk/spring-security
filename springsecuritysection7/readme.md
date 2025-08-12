040d0e6ab96474bb9bafc89b50623df15779e4c8
tarun_soodula <tarunstk8897@gmail.com>
2025-08-12 11:16:10 +0530

    Added event listeners, session fixation attacks.

--- springsecuritysection7/readme.md
+++ springsecuritysection7/readme.md
@@ -2,3 +2,5 @@
 
 - Added Custom AuthenticationEntryPoint and CustomAccessDeniedHandler classes, which take care of giving customized responses when authenticaiton or authorization exceptions occur inside the code
 - HTTPS connection to be verified
+- Add session fixation attack prevention config
+- Added event listeners to log data based on success/failure authentication
--- springsecuritysection7/src/main/java/com/stkbank/config/ProjectSecurityProdConfig.java
+++ springsecuritysection7/src/main/java/com/stkbank/config/ProjectSecurityProdConfig.java
@@ -25,13 +25,19 @@ public class ProjectSecurityProdConfig {
          *  maxSessionsPreventsLogin(true) -> If user tries to create more number of sessions */
 //        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true))
 //                http.requiresChannel(rcc -> rcc.anyRequest().requiresSecure())
-                http.csrf(csrfConfig -> csrfConfig.disable())
+        /*below config is used to avoid common session fixation attacks, that occur via session ids
+         * by default spring security handles session fixation attacks, by using changeSessionId() method
+         * but there are also other configs that you can use like, migrateSession(), changeSessionId()
+         * and also there is one moore method called none(), which you've to only use when you don't want any session fixation attack
+         * prevention from springsecurity, means you've to implement your own session fixation attack prevention logic*/
+        http.sessionManagement(session -> session.sessionFixation((sessionFixation -> sessionFixation.newSession())))
+                .csrf(csrfConfig -> csrfConfig.disable())
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                         .requestMatchers("/notices", "/contact", "/register", "/invalidSession").permitAll());
         http.formLogin(Customizer.withDefaults());
         /*Commented below cause, now we don't want default behavioru, we've our custom
-        * response created in CustomBasicAuthenticationEntryPoint, when authentication fails*/
+         * response created in CustomBasicAuthenticationEntryPoint, when authentication fails*/
 //        http.httpBasic(Customizer.withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         /*Added below, cause now we want to throw our modified custom exception message when any forbidden issues come.*/
